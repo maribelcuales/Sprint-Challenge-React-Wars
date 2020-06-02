222478f400cd5e73e9b3584321e9adf65fee8bdf
maribelcuales <maribelcuales@gmail.com>
2020-06-02 19:02:07 +0800

    Added Col, Card, CardHeader and other Reactstrap components.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,19 +1,35 @@
 // Write your Character component here
 
 import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardText,
+  CardBody,
+  CardSubtitle,
+  Badge,
+  Col
+} from "reactstrap";
 
 const Character = (props) => {
   return (
-    <div>
-      <h2>Name: {props.name}</h2>
-      <p>Born: {props.birthYear}</p>
-      <p>Height: {props.height}</p>
-      <p>Weight: {props.mass}</p>
-      <p>Hair Color: {props.hairColor}</p>
-      <p>Skin Color: {props.skinColor}</p>
-      <p>Eye Color: {props.eyeColor}</p>
-      <p>Gender: {props.gender}</p>
-    </div>
+    <Col xl="3">
+      <Card>
+        <CardHeader> 
+          {props.name} 
+        </CardHeader>
+        <CardBody>
+          <CardText> Born: {props.birthYear}
+          </CardText>
+          <CardText>Height: {props.height}</CardText>
+          <p>Weight: {props.mass}</p>
+          <p>Hair Color: {props.hairColor}</p>
+          <p>Skin Color: {props.skinColor}</p>
+          <p>Eye Color: {props.eyeColor}</p>
+          <CardSubtitle>Gender: {props.gender}</CardSubtitle>
+        </CardBody>
+      </Card>
+    </Col>
   )
 }
 
